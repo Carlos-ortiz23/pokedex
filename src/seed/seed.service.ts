1a22ea86a3a476426f07ddefaffc58569aee29ac
Carlos Ortiz <carlosor3000@gmail.com>
2026-02-21 17:17:23 -0500

    Añadir axios como dependencia y crear interfaz PokeResponse para manejar la respuesta de la API

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke.response.interface';
 
 
 @Injectable()
 export class SeedService {
+
+  private readonly axios: AxiosInstance = axios;
  
-  executeSeed() {
-    return `seed executed successfuly!!`;
+  async executeSeed() {
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+    
+    data.results.forEach(({name, url}) => {
+
+      const segments = url.split('/');
+      const no: number = +segments[6]
+
+      console.log({name, no})
+    });
+    return data.results;
   }
 
 }
